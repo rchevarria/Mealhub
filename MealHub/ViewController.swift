020c7a3ce55e46cb1fb20383cb3e600a61958a88
Ryan Chevarria <Ryan.Chevarria64@myhunter.cuny.edu>
2023-09-18 13:37:34 -0400

    Xcode Project setup along with initial libraries

---
+++ MealHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealHub
+//
+//  Created by Ryan Chevarria on 9/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
